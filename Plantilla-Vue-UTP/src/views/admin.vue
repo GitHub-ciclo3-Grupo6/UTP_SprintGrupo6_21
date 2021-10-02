466680a460769e00d74d49a9f8b3ea1044940db3
J K <camilopa1998@hotmail.com>
2021-10-02 01:13:58 -0500

    Vista proyecto integrada
    Se integro la vista del proyecto en vue

---
+++ Plantilla-Vue-UTP/src/views/admin.vue
@@ -0,0 +1,234 @@
+<template>
+  <div>
+    <header
+      class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow"
+    >
+      <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">
+        <img class="" src="@/assets/img/logo.svg" alt="" width="30" /> -
+        FerreSoft</a
+      >
+      <button
+        class="navbar-toggler position-absolute d-md-none collapsed"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#sidebarMenu"
+        aria-controls="sidebarMenu"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <input
+        class="form-control form-control-dark w-100"
+        type="text"
+        placeholder="Search"
+        aria-label="Search"
+      />
+      <div class="navbar-nav">
+        <div class="nav-item text-nowrap">
+          <a class="nav-link px-3" href="../login/index.html">Cerrar sesión</a>
+        </div>
+      </div>
+    </header>
+
+    <div class="container-fluid">
+      <div class="row">
+        <nav
+          id="sidebarMenu"
+          class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse"
+        >
+          <div class="position-sticky pt-3">
+            <ul class="nav flex-column">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">
+                  <span data-feather="home"></span>
+                  Dashboard
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  <span data-feather="file"></span>
+                  Pedidos
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  <span data-feather="shopping-cart"></span>
+                  Productos
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  <span data-feather="users"></span>
+                  Clientes
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  <span data-feather="bar-chart-2"></span>
+                  Reportes
+                </a>
+              </li>
+            </ul>
+
+            <h6
+              class="
+                sidebar-heading
+                d-flex
+                justify-content-between
+                align-items-center
+                px-3
+                mt-4
+                mb-1
+                text-muted
+              "
+            >
+              <span>Reportes guardados</span>
+              <a class="link-secondary" href="#" aria-label="Add a new report">
+                <span data-feather="plus-circle"></span>
+              </a>
+            </h6>
+            <ul class="nav flex-column mb-2">
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  <span data-feather="file-text"></span>
+                  Mes actual
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  <span data-feather="file-text"></span>
+                  6 Meses anteriores
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  <span data-feather="file-text"></span>
+                  1 Año
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+
+        <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+          <div
+            class="
+              d-flex
+              justify-content-between
+              flex-wrap flex-md-nowrap
+              align-items-center
+              pt-3
+              pb-2
+              mb-3
+              border-bottom
+            "
+          >
+            <h1 class="h2">Principal</h1>
+            <div class="btn-toolbar mb-2 mb-md-0">
+              <div class="btn-group me-2">
+                <button type="button" class="btn btn-sm btn-outline-secondary">
+                  Compartir
+                </button>
+                <button type="button" class="btn btn-sm btn-outline-secondary">
+                  Exportar
+                </button>
+              </div>
+              <button
+                type="button"
+                class="btn btn-sm btn-outline-secondary dropdown-toggle"
+              >
+                <span data-feather="calendar"></span>
+                Esta semana
+              </button>
+            </div>
+          </div>
+          <div class="table-responsive">
+            <table class="table table-striped table-sm">
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Header</th>
+                  <th scope="col">Header</th>
+                  <th scope="col">Header</th>
+                  <th scope="col">Header</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1,001</td>
+                  <td>random</td>
+                  <td>data</td>
+                  <td>placeholder</td>
+                  <td>text</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </main>
+      </div>
+    </div>
+  </div>
+  <!-- <div class="admin">
+    <b-nav :class="[ !visibleSidebar ? 'd-none' : '' ]" vertical class="sidebar-content">
+      <div>
+        <h1>Menu</h1>
+      </div>
+      <b-nav-item @click="changeOption(1)" :class="[ option==1 ? 'active' : '']">Categorias</b-nav-item>
+      <b-nav-item @click="changeOption(2)" :class="[ option==2 ? 'active' : '']">Articulos</b-nav-item>
+      <b-nav-item @click="changeOption(3)" :class="[ option==3 ? 'active' : '']">Usuarios</b-nav-item>
+    </b-nav>
+    <div class="content-custom">
+      <b-navbar type="dark" variant="dark">
+        <div class="d-flex align-items-center mr-3">
+          <span @click="isVisibleSidebar" class="navbar-toggler-icon"></span>
+        </div>
+        <b-navbar-nav>          
+          <router-link to="/" class="nav-link">Home</router-link>          
+        </b-navbar-nav>
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item-dropdown text="User" right>
+            <b-dropdown-item href="#">Account</b-dropdown-item>
+            <b-dropdown-item><router-link to="/login">logout</router-link></b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-navbar>
+      <div class="container-fluid">
+        <div class="m-5">
+          <Category v-if="option == 1"/>
+          <Article v-else-if="option == 2"/>
+          <User v-else-if="option == 3"/>
+        </div>
+      </div>
+    </div>
+  </div> -->
+</template>
+<script>
+/* import Category from "../components/admin/category.vue";
+import Article from "../components/admin/article.vue";
+import User from "../components/admin/user.vue"; */
+export default {
+  components: {
+    /*  Category,
+    Article,
+    User, */
+  },
+  data() {
+    return {
+      option: 1,
+      visibleSidebar: true,
+    };
+  },
+  methods: {
+    changeOption(i) {
+      this.option = i;
+    },
+    isVisibleSidebar() {
+      this.visibleSidebar = !this.visibleSidebar;
+    },
+  },
+};
+</script>
+<style scoped>
+@import "../assets/css/dashboard.css";
+</style>
---
+++ Plantilla-Vue-UTP/src/views/login.vue
@@ -0,0 +1,119 @@
+<template>
+  <main class="form-signin">
+    <form>
+      <img class="mb-4" src="@/assets/img/logo.svg" alt="" width="72" />
+      <h1 class="h3 mb-3 fw-normal">Favor Inicie sesión</h1>
+
+      <div class="form-floating">
+        <input
+          type="email"
+          class="form-control"
+          id="floatingInput"
+          placeholder="name@example.com"
+        />
+        <label for="floatingInput">Correo</label>
+      </div>
+      <div class="form-floating">
+        <input
+          type="password"
+          class="form-control"
+          id="floatingPassword"
+          placeholder="Password"
+        />
+        <label for="floatingPassword">Contraseña</label>
+      </div>
+
+      <div class="checkbox mb-3">
+        <label> <input type="checkbox" value="remember-me" /> Recordar </label>
+      </div>
+      <!--       <button class="w-100 btn btn-lg btn-primary" type="submit">Iniciar sesión</button> -->
+      <a href="../admin/index.html" class="w-100 btn btn-lg btn-primary"
+        >Iniciar sesión</a
+      >
+      <p class="mt-3 mb-5 mb-3 text-muted">&copy; 2017–2021</p>
+    </form>
+    <a href="../index.html" class="w-100 btn btn-lg btn-danger"
+      >Volver a Inicio</a
+    >
+  </main>
+  <!-- <div class="login">
+    <div class="mt-5 login-content">
+      <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+        <b-form-group
+          id="input-group-1"
+          label="Email address:"
+          label-for="input-1"
+          description="We'll never share your email with anyone else."
+        >
+          <b-form-input
+            id="input-1"
+            v-model="form.email"
+            type="email"
+            placeholder="Enter email"
+            required
+          ></b-form-input>
+        </b-form-group>
+
+        <b-form-group id="input-group-2" label="Password:" label-for="input-2">
+          <b-form-input
+            id="input-2"
+            v-model="form.name"
+            type="password"
+            required
+          ></b-form-input>
+        </b-form-group>        
+
+        <b-form-group id="input-group-4" v-slot="{ ariaDescribedby }">
+          <b-form-checkbox-group
+            v-model="form.checked"
+            id="checkboxes-4"
+            :aria-describedby="ariaDescribedby"
+          >
+            <b-form-checkbox value="me">Check me out</b-form-checkbox>
+            <b-form-checkbox value="that">Check that out</b-form-checkbox>
+          </b-form-checkbox-group>
+        </b-form-group>
+
+        <b-button type="submit" variant="primary" class="mr-2">Submit</b-button>
+        <b-button type="reset" variant="danger">Regresar</b-button>
+      </b-form>      
+    </div>
+  </div> -->
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        email: "",
+        name: "",
+        food: null,
+        checked: [],
+      },
+      foods: [
+        { text: "Select One", value: null },
+        "Carrots",
+        "Beans",
+        "Tomatoes",
+        "Corn",
+      ],
+      show: true,
+    };
+  },
+  methods: {
+    onSubmit(event) {
+      event.preventDefault();
+      alert(JSON.stringify(this.form));
+      this.$router.push("admin");
+    },
+    onReset(event) {
+      event.preventDefault();
+      this.$router.push("/");
+    },
+  },
+};
+</script>
+<style scoped>
+@import "../assets/css/login.css";
+</style>

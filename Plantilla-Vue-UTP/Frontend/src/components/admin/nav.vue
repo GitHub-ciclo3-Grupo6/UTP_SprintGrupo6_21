54ebfab3b3e80de5c831314af4547e71f890dceb
J K <camilopa1998@hotmail.com>
2021-10-04 23:26:55 -0500

    Views creadas
    Se crearon las vistas para diferentes componentes

---
+++ Plantilla-Vue-UTP/Frontend/src/components/admin/nav.vue
@@ -0,0 +1,139 @@
+<template>
+  <nav
+    id="sidebarMenu"
+    class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse"
+  >
+    <div class="position-sticky pt-3">
+      <ul class="nav flex-column">
+        <li class="nav-item">
+          <a
+            class="nav-link"
+            @click="$emit('update:option', 1)"
+            :class="[option == 1 ? 'active' : '']"
+            aria-current="page"
+            href="#"
+          >
+            <span data-feather="home"></span>
+            Dashboard
+          </a>
+        </li>
+        <li class="nav-item">
+          <a
+            class="nav-link"
+            @click="$emit('update:option', 2)"
+            :class="[option == 2 ? 'active' : '']"
+            href="#"
+          >
+            <span data-feather="file"></span>
+            Pedidos
+          </a>
+        </li>
+        <li class="nav-item">
+          <a
+            class="nav-link"
+            @click="$emit('update:option', 3)"
+            :class="[option == 3 ? 'active' : '']"
+            href="#"
+          >
+            <span data-feather="shopping-cart"></span>
+            Productos
+          </a>
+        </li>
+        <li class="nav-item">
+          <a
+            class="nav-link"
+            @click="$emit('update:option', 4)"
+            :class="[option == 4 ? 'active' : '']"
+            href="#"
+          >
+            <span data-feather="users"></span>
+            Clientes
+          </a>
+        </li>
+        <li class="nav-item">
+          <a
+            class="nav-link"
+            @click="$emit('update:option', 5)"
+            :class="[option == 5 ? 'active' : '']"
+            href="#"
+          >
+            <span data-feather="users"></span>
+            Usuarios
+          </a>
+        </li>
+        <li class="nav-item">
+          <a
+            class="nav-link"
+            @click="$emit('update:option', 6)"
+            :class="[option == 6 ? 'active' : '']"
+            href="#"
+          >
+            <span data-feather="bar-chart-2"></span>
+            Reportes
+          </a>
+        </li>
+      </ul>
+
+      <h6
+        class="
+          sidebar-heading
+          d-flex
+          justify-content-between
+          align-items-center
+          px-3
+          mt-4
+          mb-1
+          text-muted
+        "
+      >
+        <span>Reportes guardados</span>
+        <a class="link-secondary" href="#" aria-label="Add a new report">
+          <span data-feather="plus-circle"></span>
+        </a>
+      </h6>
+      <ul class="nav flex-column mb-2">
+        <li class="nav-item">
+          <a
+            class="nav-link"
+            @click="$emit('update:option', 7)"
+            :class="[option == 7 ? 'active' : '']"
+            href="#"
+          >
+            <span data-feather="file-text"></span>
+            Mes actual
+          </a>
+        </li>
+        <li class="nav-item">
+          <a
+            class="nav-link"
+            @click="$emit('update:option', 8)"
+            :class="[option == 8 ? 'active' : '']"
+            href="#"
+          >
+            <span data-feather="file-text"></span>
+            6 Meses anteriores
+          </a>
+        </li>
+        <li class="nav-item">
+          <a
+            class="nav-link"
+            @click="$emit('update:option', 9)"
+            :class="[option == 9 ? 'active' : '']"
+            href="#"
+          >
+            <span data-feather="file-text"></span>
+            1 Año
+          </a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+<script>
+export default {
+  name: "NavAdmin",
+  props: {
+    option: Number,
+  },
+};
+</script>
--- Plantilla-Vue-UTP/Frontend/src/views/admin.vue
+++ Plantilla-Vue-UTP/Frontend/src/views/admin.vue
@@ -1,172 +1,16 @@
 <template>
   <div>
-    <header
-      class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow"
-    >
-      <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">
-        <img class="" src="@/assets/img/logo.svg" alt="" width="30" /> -
-        FerreSoft</a
-      >
-      <button
-        class="navbar-toggler position-absolute d-md-none collapsed"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#sidebarMenu"
-        aria-controls="sidebarMenu"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <input
-        class="form-control form-control-dark w-100"
-        type="text"
-        placeholder="Search"
-        aria-label="Search"
-      />
-      <div class="navbar-nav">
-        <div class="nav-item text-nowrap">
-          <a class="nav-link px-3" href="" @click="cerrarSesion()">
-            Cerrar sesión
-          </a>
-        </div>
-      </div>
-    </header>
+    <HeaderAdmin></HeaderAdmin>
 
     <div class="container-fluid">
       <div class="row">
-        <nav
-          id="sidebarMenu"
-          class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse"
-        >
-          <div class="position-sticky pt-3">
-            <ul class="nav flex-column">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">
-                  <span data-feather="home"></span>
-                  Dashboard
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <span data-feather="file"></span>
-                  Pedidos
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <span data-feather="shopping-cart"></span>
-                  Productos
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <span data-feather="users"></span>
-                  Clientes
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <span data-feather="bar-chart-2"></span>
-                  Reportes
-                </a>
-              </li>
-            </ul>
-
-            <h6
-              class="
-                sidebar-heading
-                d-flex
-                justify-content-between
-                align-items-center
-                px-3
-                mt-4
-                mb-1
-                text-muted
-              "
-            >
-              <span>Reportes guardados</span>
-              <a class="link-secondary" href="#" aria-label="Add a new report">
-                <span data-feather="plus-circle"></span>
-              </a>
-            </h6>
-            <ul class="nav flex-column mb-2">
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <span data-feather="file-text"></span>
-                  Mes actual
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <span data-feather="file-text"></span>
-                  6 Meses anteriores
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <span data-feather="file-text"></span>
-                  1 Año
-                </a>
-              </li>
-            </ul>
-          </div>
-        </nav>
-
+        <NavAdmin :option.sync="option"></NavAdmin>
         <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-          <div
-            class="
-              d-flex
-              justify-content-between
-              flex-wrap flex-md-nowrap
-              align-items-center
-              pt-3
-              pb-2
-              mb-3
-              border-bottom
-            "
-          >
-            <h1 class="h2">Principal</h1>
-            <div class="btn-toolbar mb-2 mb-md-0">
-              <div class="btn-group me-2">
-                <button type="button" class="btn btn-sm btn-outline-secondary">
-                  Compartir
-                </button>
-                <button type="button" class="btn btn-sm btn-outline-secondary">
-                  Exportar
-                </button>
-              </div>
-              <button
-                type="button"
-                class="btn btn-sm btn-outline-secondary dropdown-toggle"
-              >
-                <span data-feather="calendar"></span>
-                Esta semana
-              </button>
-            </div>
-          </div>
-          <div class="table-responsive">
-            <table class="table table-striped table-sm">
-              <thead>
-                <tr>
-                  <th scope="col">#</th>
-                  <th scope="col">Header</th>
-                  <th scope="col">Header</th>
-                  <th scope="col">Header</th>
-                  <th scope="col">Header</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>1,001</td>
-                  <td>random</td>
-                  <td>data</td>
-                  <td>placeholder</td>
-                  <td>text</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
+          <Dashboard v-if="option == 1"></Dashboard>
+          <Pedido v-else-if="option == 2"></Pedido>
+          <Producto v-else-if="option == 3"></Producto>
+          <Cliente v-else-if="option == 4"></Cliente>
+          <Usuario v-else-if="option == 5"></Usuario>
         </main>
       </div>
     </div>
@@ -209,10 +53,23 @@
 /* import Category from "../components/admin/category.vue";
 import Article from "../components/admin/article.vue";
 import User from "../components/admin/user.vue"; */
+import HeaderAdmin from "@/components/admin/header.vue";
+import NavAdmin from "@/components/admin/nav.vue";
+import Dashboard from "@/components/admin/body/dashboard.vue";
+import Pedido from "@/components/admin/body/pedido.vue";
+import Producto from "@/components/admin/body/producto.vue";
+import Cliente from "@/components/admin/body/cliente.vue";
+import Usuario from "@/components/admin/body/usuario.vue";
 
-import { mapActions } from "vuex";
 export default {
   components: {
+    HeaderAdmin,
+    NavAdmin,
+    Dashboard,
+    Pedido,
+    Producto,
+    Cliente,
+    Usuario,
     /*  Category,
     Article,
     User, */
@@ -224,7 +81,6 @@ export default {
     };
   },
   methods: {
-    ...mapActions(["cerrarSesion"]),
     changeOption(i) {
       this.option = i;
     },
